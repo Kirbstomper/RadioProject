983092451f3dba0841fd863af322f8388a87ec2f
Kirbstomper <WingedKraby@gmail.com>
2017-02-23 12:48:16 -0500

    Basic HTML and started Javascript

---
+++ playSongs.js
@@ -0,0 +1,27 @@
+/*Author: CHristopher SMith
+ Rationale: control the uploading process
+*/
+
+// Variables
+var Songs = [];
+
+/*
+	I will need an array of string which will be the file names
+	Though if scaling I should prob use some database
+
+*/
+//THis is the main function that will be called on the page
+
+function playRadio{
+
+}
+
+//Populates the song list
+//Useful for when someone uploaded a new song
+function populateSongs{
+
+}
+//Uploads a new song and populates the list
+function uploadSong{
+	get
+}
