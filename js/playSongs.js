22e6bfe7015ec8c5f1d6c758417b8ec746a6a4dd
Christopher Smith <christopher@tinktank.dyn6.twc.com>
2017-02-24 02:15:56 -0500

    Added file uploading, still need to check if a file is a real MP3 or not

---
+++ js/playSongs.js
@@ -0,0 +1,24 @@
+/*Author: CHristopher SMith
+ Rationale: Display song information
+*/
+ 
+
+/*
+	
+
+*/
+//THis is the main function that will be called on the page
+
+function playRadio{
+
+}
+
+//Populates the song list
+//Useful for when someone uploaded a new song
+function populateSongs{
+
+}
+//Uploads a new song and populates the list
+function uploadSong{
+	get
+}
